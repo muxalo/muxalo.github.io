9a281f980ec0cdd17a1566e9487f97ee893dea2e
Mikhail Kanashevich <muxalo@yandex.ru>
2016-09-22 22:40:07 +0300

    add new method for insert HTML to page

--- hw_js/homework_3-4/js/script.js
+++ hw_js/homework_3-4/js/script.js
@@ -1,6 +1,6 @@
 
 	//	Вариант создания страницы №1
-	
+
 /*
 
 var object = document.body;
@@ -66,69 +66,69 @@ var j = 0;
 var parentElemList = [];
 
 for ( i = 0; i < amountOfQuestions; i++ ) {
-	
+
 		// Create elements 'li' in list 'ol'
-	
+
 		var _listItem = document.createElement('li');
-		
+
 		parentElemList[i] = _tempParentElem.insertBefore(_listItem, null);
-		
+
 		// Create element 'p' in list item 'li'
-		
+
 		var _paragraph = document.createElement('p');
-		
+
 		_paragraph.style.margin = '15px 0';
 		_paragraph.innerHTML = 'Вопрос №' + (i + 1);
-		
+
 		parentElemList[i].insertBefore(_paragraph, null);
-		
+
 		// Create element 'ul' in list item 'li'
-		
+
 		var _ulList = document.createElement('ul');
-		
+
 		_ulList.style.fontSize = '16px';
-		
+
 		var tempParent = parentElemList[i].insertBefore(_ulList, null);
-		
+
 		for ( j = 0; j < amountOfAnswers; j++ ) {
-			
+
 			// Create element 'li' in list 'ul'
-			
+
 			var _tempElem = document.createElement('li');
-			
+
 			_tempElem.className = 'list-inline';
-			
+
 			var tempParent1 = tempParent.insertBefore(_tempElem, null);
-			
+
 			// Create element 'label' in list item 'li' of 'ul'
-			
+
 			_tempElem = document.createElement('label');
-			
+
 			tempParent1 = tempParent1.insertBefore(_tempElem, null);
-			
+
 			// Create element 'input' in 'label'
-			
+
 			_tempElem = document.createElement('input');
-			
+
 			_tempElem.type = 'radio';
 			_tempElem.name = 'question_' + (i + 1);
 			_tempElem.value = j + 1;
-			
+
 			tempParent1.insertBefore(_tempElem, null);
-			
+
 			// Create element 'span' in 'label'
-			
+
 			_tempElem = document.createElement('span');
-			
+
 			_tempElem.style = 'margin-left: 5px';
 			_tempElem.innerHTML = 'Вариант ответа №' + (j + 1);
-			
+
 			tempParent1.insertBefore(_tempElem, null);
 		}
 	}
-	
+
 // Set parent for button ('form')
-	
+
 parentElem = document.getElementsByTagName('form');
 
 // Create element 'p' in 'form'
@@ -155,3 +155,12 @@ parentElem.insertBefore(_tempElem, null);
 
 	//	Вариант создания страницы №3
 
+var body = document.body;
+
+var insert = function() {
+	this.innerHTML = '<div class="container"><div class="jumbotron"><h1 style="text-align: center">Тест по программированию</h1><form method="post"><ol style="font-size: 21px"><li><p style="margin: 15px 0">Вопрос №1</p><ul style="font-size: 16px"><li class="list-inline"><label><input type="radio" name="question_1" value="1"><span style="margin-left: 5px">Вариант ответа №1</span></label></li><li class="list-inline"><label><input type="radio" name="question_1" value="2"><span style="margin-left: 5px">Вариант ответа №2</span></label></li><li class="list-inline"><label><input type="radio" name="question_1" value="3"><span style="margin-left: 5px">Вариант ответа №3</span></label></li></ul></li><li><p style="margin: 15px 0">Вопрос №2</p><ul style="font-size: 16px"><li class="list-inline"><label><input type="radio" name="question_2" value="1"><span style="margin-left: 5px">Вариант ответа №1</span></label></li><li class="list-inline"><label><input type="radio" name="question_2" value="2"><span style="margin-left: 5px">Вариант ответа №2</span></label></li><li class="list-inline"><label><input type="radio" name="question_2" value="3"><span style="margin-left: 5px">Вариант ответа №3</span></label></li></ul></li><li><p style="margin: 15px 0">Вопрос №3</p><ul style="font-size: 16px"><li class="list-inline"><label><input type="radio" name="question_3" value="1"><span style="margin-left: 5px">Вариант ответа №1</span></label></li><li class="list-inline"><label><input type="radio" name="question_3" value="2"><span style="margin-left: 5px">Вариант ответа №2</span></label></li><li class="list-inline"><label><input type="radio" name="question_3" value="3"><span style="margin-left: 5px">Вариант ответа №3</span></label></li></ul></li></ol><p style="text-align: center"><a class="btn btn-lg btn-info" href="#" role="button">Проверить мои результаты</a></p></form></div></div>';
+}
+
+body.insert = insert;
+
+body.insert();
