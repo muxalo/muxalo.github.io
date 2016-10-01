ee110ff48956ddb793d3d4b3b91dc6f0233b5f2e
Mikhail Kanashevich <muxalo@yandex.ru>
2016-10-01 08:35:52 +0300

    add new version of js homework 5-6 b apdate main page

--- hw_js/homework_5-6/js/script.js
+++ hw_js/homework_5-6/js/script.js
@@ -1,16 +1,16 @@
 start.addEventListener("click", start_click);
-pause.addEventListener("click", pause_click);
-stp.addEventListener("click", stp_click);
+stp.addEventListener("click", stop_click);
+split.addEventListener("click", split_click);
+reset.addEventListener("click", reset_click);
 
-var count = 0, timerId, go = false;
+var count = 0, timerId, go = false, round = 0;
 
 for_count.innerHTML = "00:00:00:00";
 
-pause.style = "display: none;"
+stp.style = "display: none;"
 
 function start_click() {
   if ( go == false ) {
-    console.log('Start click');
     timerId = setInterval( function() {
       count++;
       var msec = count % 100;
@@ -21,31 +21,54 @@ function start_click() {
       if ( min < 10 ) { min = "0" + min; }
       var hours = Math.floor( count / 360000 ) % 24;
       if ( hours < 10 ) { hours = "0" + hours; }
-      var time = hours + ":" + min + ":" + sec + ":" + msec;
+      time = hours + ":" + min + ":" + sec + ":" + msec;
       for_count.innerHTML = time;
     }, 1);
     go = true;
     start.style = "display: none;"
-    pause.style = "display: inline-block;"
+    stp.style = "display: inline-block;"
+    console.log('Start click');
   }
 }
 
-function pause_click() {
+function stop_click() {
   if (go == true) {
-    console.log('Pause click');
   	clearInterval(timerId);
     go = false;
+    round++;
+    var tempElem = document.createElement('li');
+    tempElem.classList = "splits__item";
+    tempElem.innerHTML = round + ' stop : ' + time;
+    parentElem[0].appendChild(tempElem);
     start.style = "display: inline-block;"
-    pause.style = "display: none;"
+    stp.style = "display: none;"
+    console.log('Stop click');
   }
 }
 
-function stp_click() {
-  console.log('Stop click');
+var parentElem = document.getElementsByClassName('splits');
+
+function split_click() {
+  if ( go == true ) {
+    round++;
+    var tempElem = document.createElement('li');
+    tempElem.classList = "splits__item";
+    tempElem.innerHTML = round + ' round : ' + time;
+    parentElem[0].appendChild(tempElem);
+    console.log('Split click', round);
+  }
+}
+
+function reset_click() {
 	clearInterval(timerId);
-	count=0;
+	count = 0;
+  round = 0;
   go = false;
   start.style = "display: inline-block;"
-  pause.style = "display: none;"
+  stp.style = "display: none;"
 	for_count.innerHTML = "00:00:00:00";
+  while (parentElem[0].lastChild) {
+    parentElem[0].removeChild(parentElem[0].lastChild);
+  }
+  console.log('Reset click');
 }
