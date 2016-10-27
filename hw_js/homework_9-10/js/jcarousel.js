330819d30d5edb11b25bdd9c803f0e5fc175bde0
Mikhail Kanashevich <muxalo@yandex.ru>
2016-10-27 19:11:41 +0300

    Add part of homework js9-10

---
+++ hw_js/homework_9-10/js/jcarousel.js
@@ -0,0 +1,57 @@
+(function($) {
+    $(function() {
+        /*
+        Carousel initialization
+        */
+        $('.jcarousel')
+            .jcarousel({
+                // Options go here
+            });
+
+        /*
+         Prev control initialization
+         */
+        $('.jcarousel-control-prev')
+			.html('‹')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '-=1'
+            });
+
+        /*
+         Next control initialization
+         */
+        $('.jcarousel-control-next')
+			.html('›')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '+=1'
+            });
+
+        /*
+         Pagination initialization
+         */
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination({
+                // Options go here
+            });
+    });
+})(jQuery);
